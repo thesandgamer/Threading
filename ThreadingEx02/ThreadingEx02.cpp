07aefa60f3971baa7b7e159a405660a062f1186b
NOURIGAT Adrien <adrien.nourigat@artfx34.lan>
2023-02-14 12:12:36 +0100

    add part 2

--- ThreadingEx02/ThreadingEx02.cpp
+++ ThreadingEx02/ThreadingEx02.cpp
@@ -1,26 +1,53 @@
 #include <iostream>
 #include <thread>
 #include <vector>
+#include <mutex>
+#include <algorithm>
+#include <list>
 
 using namespace std;
 using std::cout;
 using std::endl;
 
 
+
+int FinalSum;
+std::mutex m;
+std::mutex m2;
+
 int SumList(vector<int> list)
 {
+	std::lock_guard<std::mutex> guard(m);
 	int sum{ 0 };
 	for (int var : list)
 	{
 		sum += var;
 	}
+	FinalSum += sum;
+
 	return sum;
 }
+
+void DisplayOddNumbers()
+{
+	std::lock_guard<std::mutex> guard(m2);
+
+	for (size_t i = 0; i <= 1000; i++)
+	{
+		if (i % 2 == 0)
+		{
+			cout << i << "\n";
+		}
+	}
+}
+
 
 int main() {
 
 	vector<int> list{ 1,2,3,4,5,6,7};
 	int sum {0};
+
+	vector <std::thread> threads;
 
 
 	
@@ -40,9 +67,18 @@ int main() {
 	//Get Sum for all sub lits
 	for (vector<int> var : sublists)
 	{
-		sum += SumList(var);
+		threads.emplace_back(
+			([=, &sum]() {
+				sum += SumList(var);
+				})
+		);
+		
 	}
 
+	for (size_t i = 0; i < threads.size(); i++)//On rejoins tout les threads
+	{
+		threads[i].join();
+	}
 
 	/*
 	for (int var : list)
@@ -51,7 +87,16 @@ int main() {
 	}
 	*/
 
-	cout << sum << endl;
+	cout << sum << "\n";
+	cout << FinalSum << "\n" << endl;
+
+	thread t1(DisplayOddNumbers);
+	thread t2(DisplayOddNumbers);
+
+	t1.join();
+	t2.join();
+
+	
 
 	return 0;
 
